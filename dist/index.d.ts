f7ed5e691d6ed460098a6eb27d53a9db599ee87a
lenonbr <lenon.br@hotmail.com>
2025-06-03 14:53:54 -0300

    atualizando t

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,2 +1,21 @@
 import BackendWorkerFinanceiro from "./backend-wroker-financeiro";
-export type t = typeof BackendWorkerFinanceiro;
+declare namespace t {
+    export import Financeiro = BackendWorkerFinanceiro;
+
+    export import Bucket = BackendWorkerFinanceiro;
+
+    export import Wave = BackendWorkerFinanceiro;
+
+    export import Portal = BackendWorkerFinanceiro;
+
+    export import Locatario = BackendWorkerFinanceiro;
+
+    export import Analisando = BackendWorkerFinanceiro;
+
+    export import Banco = BackendWorkerFinanceiro;
+}
+
+export default t;
+
+
+
