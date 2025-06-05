864d4c8d29688ffd6be9e224ce2a21740f2a7bd1
theGuil <seflanguilhermesouza@gmail.com>
2025-06-05 00:01:40 -0300

    ⚡ perf:

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,15 @@
 // import BackendWorkerFinanceiro from "./backend-worker-financeiro";
 import BackendBanco from "./backend-banco";
 
-const t = {
+namespace t {
     // Financeiro: BackendWorkerFinanceiro,
-    Banco: BackendBanco,
+
+    export import Banco = BackendBanco;
     // Bucket: BackendWorkerFinanceiro,
     // Wave: BackendWorkerFinanceiro,
     // Portal: BackendWorkerFinanceiro,
     // Locatario: BackendWorkerFinanceiro,
     // Analisando: BackendWorkerFinanceiro,
-};
+}
 
 export default t;
