eb49b2d21b19513d8825233bc26a2b8a5f5a4b7c
theGuil <seflanguilhermesouza@gmail.com>
2025-06-05 20:22:41 -0300

    ÔÜí perf: Edita onda-types

--- src/backend-banco/helpers.ts
+++ src/backend-banco/helpers.ts
@@ -12,11 +12,11 @@ namespace ControllerHelpers {
     // Schema base para o helper
     export const HelperBaseSchema = z4.object({
         _id: z4.uuid(),
-        data_criacao: z4.string(),
-        data_atualizacao: z4.string().nullable(),
+        data_criacao: z4.date(),
+        data_atualizacao: z4.date().nullable(),
         usuario_create_id: z4.uuidv4(),
-        descricao: z4.string(),
-        entidade: z4.string(),
+        descricao: z4.string().toLowerCase(),
+        entidade: z4.string().toLowerCase(),
         ativo: z4.boolean()
     });
     export type HelperBase = z4.infer<typeof HelperBaseSchema>;
@@ -25,8 +25,8 @@ namespace ControllerHelpers {
         export const InputSchema = z4.object({
             data: z4.object({
                 helper: z4.object({
-                    descricao: z4.string(),
-                    entidade: z4.string(),
+                    descricao: z4.string().toLowerCase(),
+                    entidade: z4.string().toLowerCase(),
                     ativo: z4.boolean().optional().default(true),
                 })
             })
@@ -46,8 +46,8 @@ namespace ControllerHelpers {
             filtros: z4.object({
                 helper: z4.object({
                     _id: z4.uuidv4().optional(),
-                    descricao: z4.string().optional(),
-                    entidade: z4.string().optional(),
+                    descricao: z4.string().toLowerCase(),
+                    entidade: z4.string().toLowerCase(),
                     ativo: z4.boolean().optional(),
                     usuario_create_id: z4.uuidv4().optional()
                 })
@@ -84,8 +84,8 @@ namespace ControllerHelpers {
             data: z4.object({
                 helper: z4.object({
                     _id: z4.uuidv4(),
-                    descricao: z4.string().optional(),
-                    entidade: z4.string().optional(),
+                    descricao: z4.string().toLowerCase(),
+                    entidade: z4.string().toLowerCase(),
                     ativo: z4.boolean().optional()
                 })
             })
