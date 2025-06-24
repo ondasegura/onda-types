31158947aecce80de16bc78052778e44fd305c9d
iJulians <silveirajuliano7991@gmail.com>
2025-06-24 16:09:54 -0300

    :recycle: refactor: altara objeto inteiro de ControllerContasReceber

--- src/backend-worker-financeiro/controllers/contas_a_receber.ts
+++ src/backend-worker-financeiro/controllers/contas_a_receber.ts
@@ -3,170 +3,171 @@ import z4 from "zod/v4";
 // tipagem:
 // COMO USAR ESE NAMESPACE NA HORA DE IMPORTAR: 
 // import t from "onda-types"
-// t.Financeiro.Controllers.ContasReceber.Criar.Input
-namespace ControllerContasReceber {
+// t.Financeiro.Controllers.ContaReceber.Criar.Input
+namespace ControllerContaReceber {
 
-    export const ContasReceberCheckoutSchema = z4.string();
-    export type ContasReceberCheckout = z4.infer<typeof ContasReceberCheckoutSchema>;
+   export const ContaReceberCheckoutSchema = z4.string();
+   export type ContaReceberCheckout = z4.infer<typeof ContaReceberCheckoutSchema>;
 
-    export const ContasReceberMetodoPagamentoSchema = z4.array(z4.string());
-    export type ContasReceberMetodoPagamento = z4.infer<typeof ContasReceberMetodoPagamentoSchema>;
+   export const ContaReceberMetodoPagamentoSchema = z4.array(z4.string());
+   export type ContaReceberMetodoPagamento = z4.infer<typeof ContaReceberMetodoPagamentoSchema>;
 
-    export const ContasReceberBaseSchema = z4.object({
-        _id: z4.uuid(),
-        data_criacao: z4.date(),
-        data_atualizacao: z4.date().nullable(),
-        usuario_create_id: z4.uuidv4(),
-        checkout: z4.string(),
-        cliente_id: z4.string(),
-        parcelas: z4.number(),
-        valor: z4.number(),
-        vencimento: z4.string(),
-        codigo: z4.string(),
-        metodo_pagamento: z4.array(z4.string()),
-        tipo_pagamento: z4.number(),
-        descricao: z4.string(),
-        referencia_externa_primaria: z4.string(),
-        referencia_externa_secundaria: z4.string(),
-        referencia_externa_terciaria: z4.string(),
-        referencia_externa_quartenaria: z4.string(),
-        ativo: z4.boolean()
-    });
-    export type ContasReceberBase = z4.infer<typeof ContasReceberBaseSchema>;
+   export const ContaReceberBaseSchema = z4.object({
+       _id: z4.uuid(),
+       data_criacao: z4.date(),
+       data_atualizacao: z4.date().nullable(),
+       usuario_create_id: z4.uuidv4(),
+       checkout: z4.string(),
+       cliente_id: z4.string(),
+       parcelas: z4.number(),
+       valor: z4.number(),
+       vencimento: z4.string(),
+       codigo: z4.string(),
+       metodo_pagamento: z4.array(z4.string()),
+       tipo_pagamento: z4.number(),
+       descricao: z4.string(),
+       referencia_externa_primaria: z4.string(),
+       referencia_externa_secundaria: z4.string(),
+       referencia_externa_terciaria: z4.string(),
+       referencia_externa_quartenaria: z4.string(),
+       ativo: z4.boolean()
+   });
+   export type ContaReceberBase = z4.infer<typeof ContaReceberBaseSchema>;
 
-    export namespace Criar {
-        export const InputSchema = z4.object({
-            data: z4.object({
-                contas_receber: z4.object({
-                    checkout: z4.string(),
-                    cliente_id: z4.string(),
-                    parcelas: z4.number(),
-                    valor: z4.number(),
-                    vencimento: z4.string(),
-                    codigo: z4.string(),
-                    metodo_pagamento: z4.array(z4.string()),
-                    tipo_pagamento: z4.number(),
-                    descricao: z4.string(),
-                    referencia_externa_primaria: z4.string(),
-                    referencia_externa_secundaria: z4.string(),
-                    referencia_externa_terciaria: z4.string(),
-                    referencia_externa_quartenaria: z4.string(),
-                    ativo: z4.boolean().optional().default(true),
-                })
-            })
-        });
-        export type Input = z4.infer<typeof InputSchema>;
+   export namespace Criar {
+       export const InputSchema = z4.object({
+           data: z4.object({
+               conta_receber: z4.object({
+                   checkout: z4.string(),
+                   cliente_id: z4.string(),
+                   parcelas: z4.number(),
+                   valor: z4.number(),
+                   vencimento: z4.iso.datetime().optional(),
+                   codigo: z4.string(),
+                   metodo_pagamento: z4.array(z4.string()),
+                   tipo_pagamento: z4.number(),
+                   descricao: z4.string(),
+                   referencia_externa_primaria: z4.string(),
+                   referencia_externa_secundaria: z4.string(),
+                   referencia_externa_terciaria: z4.string(),
+                   referencia_externa_quartenaria: z4.string(),
+                   ativo: z4.boolean().optional().default(true),
+               })
+           })
+       });
+       export type Input = z4.infer<typeof InputSchema>;
 
-        export const OutputSchema = ContasReceberBaseSchema;
-        export type Output = {
-            data: {
-                contas_receber: z4.infer<typeof OutputSchema>;
-            }
-        }
-    }
+       export const OutputSchema = ContaReceberBaseSchema;
+       export type Output = {
+           data: {
+               conta_receber: z4.infer<typeof OutputSchema>;
+           }
+       }
+   }
 
-    export namespace BuscarPeloFiltro {
-        export const InputSchema = z4.object({
-            filtros: z4.object({
-                contas_receber: z4.object({
-                    pagina: z4.number().min(0),
-                    _id: z4.uuidv4().optional().nullable(),
-                    checkout: z4.string().optional().nullable(),
-                    cliente_id: z4.string().optional().nullable(),
-                    parcelas: z4.number().optional().nullable(),
-                    valor: z4.number().optional().nullable(),
-                    vencimento: z4.string().optional().nullable(),
-                    codigo: z4.string().optional().nullable(),
-                    metodo_pagamento: z4.array(z4.string()).optional().nullable(),
-                    tipo_pagamento: z4.number().optional().nullable(),
-                    descricao: z4.string().optional().nullable(),
-                    referencia_externa_primaria: z4.string().optional().nullable(),
-                    referencia_externa_secundaria: z4.string().optional().nullable(),
-                    referencia_externa_terciaria: z4.string().optional().nullable(),
-                    referencia_externa_quartenaria: z4.string().optional().nullable(),
-                    usuario_create_id: z4.uuidv4().optional().nullable(),
-                }),
+   export namespace BuscarPeloFiltro {
+       export const InputSchema = z4.object({
+           filtros: z4.object({
+               conta_receber: z4.object({
+                   pagina: z4.number().min(0),
+                   _id: z4.uuidv4().optional().nullable(),
+                   checkout: z4.string().optional().nullable(),
+                   cliente_id: z4.string().optional().nullable(),
+                   parcelas: z4.number().optional().nullable(),
+                   valor: z4.number().optional().nullable(),
+                   vencimento: z4.string().optional().nullable(),
+                   codigo: z4.string().optional().nullable(),
+                   metodo_pagamento: z4.array(z4.string()).optional().nullable(),
+                   tipo_pagamento: z4.number().optional().nullable(),
+                   descricao: z4.string().optional().nullable(),
+                   referencia_externa_primaria: z4.string().optional().nullable(),
+                   referencia_externa_secundaria: z4.string().optional().nullable(),
+                   referencia_externa_terciaria: z4.string().optional().nullable(),
+                   referencia_externa_quartenaria: z4.string().optional().nullable(),
+                   ativo: z4.boolean().optional().nullable(),
+                   usuario_create_id: z4.uuidv4().optional().nullable(),
+               }),
 
-            })
-        });
+           })
+       });
 
-        export type Input = z4.infer<typeof InputSchema>;
+       export type Input = z4.infer<typeof InputSchema>;
 
-        export const OutputSchema = z4.array(ContasReceberBaseSchema);
-        export type Output = {
-            data: {
-                paginacao: {
-                    total_itens: number;
-                    total_paginas: number;
-                    itens_por_pagina: number;
-                    total_itens_pagina_atual: number;
-                },
-                contas_receber: z4.infer<typeof OutputSchema>;
-            }
-        }
-    }
+       export const OutputSchema = z4.array(ContaReceberBaseSchema);
+       export type Output = {
+           data: {
+               paginacao: {
+                   total_itens: number;
+                   total_paginas: number;
+                   itens_por_pagina: number;
+                   total_itens_pagina_atual: number;
+               },
+               conta_receber: z4.infer<typeof OutputSchema>;
+           }
+       }
+   }
 
-    export namespace BuscarPeloId {
-        export const InputSchema = z4.object({
-            data: z4.object({
-                _id: z4.uuidv4()
-            })
-        });
-        export type Input = z4.infer<typeof InputSchema>;
+   export namespace BuscarPeloId {
+       export const InputSchema = z4.object({
+           data: z4.object({
+               _id: z4.uuidv4()
+           })
+       });
+       export type Input = z4.infer<typeof InputSchema>;
 
-        export const OutputSchema = ContasReceberBaseSchema;
-        export type Output = {
-            data: {
-                contas_receber: z4.infer<typeof OutputSchema>
-            }
-        }
-    }
+       export const OutputSchema = ContaReceberBaseSchema;
+       export type Output = {
+           data: {
+               conta_receber: z4.infer<typeof OutputSchema>
+           }
+       }
+   }
 
-    export namespace AtualizarPeloId {
-        export const InputSchema = z4.object({
-            data: z4.object({
-                contas_receber: z4.object({
-                    _id: z4.uuidv4(),
-                    checkout: z4.string().optional(),
-                    cliente_id: z4.string().optional(),
-                    parcelas: z4.number().optional(),
-                    valor: z4.number().optional(),
-                    vencimento: z4.string().optional(),
-                    codigo: z4.string().optional(),
-                    metodo_pagamento: z4.array(z4.string()).optional(),
-                    tipo_pagamento: z4.number().optional(),
-                    descricao: z4.string().optional(),
-                    referencia_externa_primaria: z4.string().optional(),
-                    referencia_externa_secundaria: z4.string().optional(),
-                    referencia_externa_terciaria: z4.string().optional(),
-                    referencia_externa_quartenaria: z4.string().optional(),
-                    ativo: z4.boolean().optional()
-                })
-            })
-        });
-        export type Input = z4.infer<typeof InputSchema>;
+   export namespace AtualizarPeloId {
+       export const InputSchema = z4.object({
+           data: z4.object({
+               conta_receber: z4.object({
+                   _id: z4.uuidv4(),
+                   checkout: z4.string().optional(),
+                   cliente_id: z4.string().optional(),
+                   parcelas: z4.number().optional(),
+                   valor: z4.number().optional(),
+                   vencimento: z4.string().optional(),
+                   codigo: z4.string().optional(),
+                   metodo_pagamento: z4.array(z4.string()).optional(),
+                   tipo_pagamento: z4.number().optional(),
+                   descricao: z4.string().optional(),
+                   referencia_externa_primaria: z4.string().optional(),
+                   referencia_externa_secundaria: z4.string().optional(),
+                   referencia_externa_terciaria: z4.string().optional(),
+                   referencia_externa_quartenaria: z4.string().optional(),
+                   ativo: z4.boolean().optional()
+               })
+           })
+       });
+       export type Input = z4.infer<typeof InputSchema>;
 
-        export const OutputSchema = ContasReceberBaseSchema;
-        export type Output = {
-            data: {
-                contas_receber: z4.infer<typeof OutputSchema>
-            }
-        }
-    }
+       export const OutputSchema = ContaReceberBaseSchema;
+       export type Output = {
+           data: {
+               conta_receber: z4.infer<typeof OutputSchema>
+           }
+       }
+   }
 
-    export namespace DeletarPeloId {
-        export const InputSchema = z4.object({
-            _id: z4.string()
-        });
-        export type Input = z4.infer<typeof InputSchema>;
+   export namespace DeletarPeloId {
+       export const InputSchema = z4.object({
+           _id: z4.string()
+       });
+       export type Input = z4.infer<typeof InputSchema>;
 
-        export const OutputSchema = ContasReceberBaseSchema;
-        export type Output = {
-            data: {
-                contas_receber: {}
-            }
-        }
-    }
+       export const OutputSchema = ContaReceberBaseSchema;
+       export type Output = {
+           data: {
+               conta_receber: {}
+           }
+       }
+   }
 }
 
-export default ControllerContasReceber;
+export default ControllerContaReceber;
