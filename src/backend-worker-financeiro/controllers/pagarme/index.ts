4384f2081f5e91f8c021c593141f69b667b84043
lenonbr <lenon.br@hotmail.com>
2025-06-24 08:38:54 -0300

    ÔÜí perf: Adciona valor em onda-types

--- src/backend-worker-financeiro/controllers/pagarme/index.ts
+++ src/backend-worker-financeiro/controllers/pagarme/index.ts
@@ -1,10 +1,10 @@
 import TypeOrder from "./order";
 import TypeCustomer from "./customer";
-import TypeReciver from "./reciver";
+import ControllerRecebedor from "../recebedor/recebedor";
 namespace Pagarme {
     export import Order = TypeOrder;
     export import Customer = TypeCustomer;
-    export import Reciver = TypeReciver;
+    export import Recebedor = ControllerRecebedor;
 }
 
 export default Pagarme;
