6c7132bce1bbe18113d05a49b08704205c54c4cd
iJulians <silveirajuliano7991@gmail.com>
2025-06-26 01:27:07 -0300

    :sparkles: feat: adiciona validação no checkout para verificar se é pagarme ou asaas

--- src/backend-worker-financeiro/controllers/conta_receber.ts
+++ src/backend-worker-financeiro/controllers/conta_receber.ts
@@ -8,19 +8,28 @@ namespace ControllerContaReceber {
 
    export const ContaReceberStatusSchema = z4.union([z4.literal("ativo"), z4.literal("inativo")]);
    export type ContaReceberStatus = z4.infer<typeof ContaReceberStatusSchema>;
-
+    export const CheckoutSchema = z4.union([
+        z4.literal("pagarme"),
+        z4.literal("asaas")
+        ])
+    export const MetodoPagamentoSchema = z4.union([
+        z4.literal('credit_card'),
+        z4.literal('boleto'),
+        z4.literal('debit_card'),
+        z4.literal('pix'),
+    ])
    export const ContaReceberBaseSchema = z4.object({
        _id: z4.uuid(),
        data_criacao: z4.date(),
        data_atualizacao: z4.date().nullable(),
        usuario_create_id: z4.uuidv4(),
-       checkout: z4.string(),
+       checkout: CheckoutSchema,
        cliente_id: z4.string(),
        parcelas: z4.number(),
        valor: z4.number(),
        vencimento: z4.string(),
        codigo: z4.string(),
-       metodo_pagamento: z4.array(z4.string()),
+       metodo_pagamento: z4.array(MetodoPagamentoSchema).min(1),
        tipo_pagamento: z4.number(),
        descricao: z4.string(),
        referencia_externa_primaria: z4.string(),
