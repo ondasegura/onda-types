1f0731369d646a23254056075a141f5dfd52c63a
theGuil <seflanguilhermesouza@gmail.com>
2025-06-26 08:51:53 -0300

    ⚡ perf: adiciona type

--- src/backend-banco/services/analisando.ts
+++ src/backend-banco/services/analisando.ts
@@ -29,178 +29,180 @@ namespace ServicesAnalisando {
         };
 
         export interface Output {
-            reportName: string;
-            registration: {
-                documentNumber: string;
-                consumerName: string;
-                motherName: string;
-                consumerGender: string;
-                birthDate: string;
-                statusRegistration: string;
-                statusDate: string;
-                consumerGenderDescription: string;
-                address: {
-                    addressLine: string;
-                    district: string;
-                    zipCode: string;
-                    country: string;
-                    city: string;
-                    state: string;
-                };
-                addresses: {
-                    addressLine: string;
-                    addressTypeCode: number;
-                    addressTypeDescription: string;
-                    addressNumber: string;
-                    district: string;
-                    zipCode: string;
-                    country: string;
-                    city: string;
-                    state: string;
-                    addressComplement: string;
-                    updateDate: string;
-                }[];
-                phones: {
-                    regionCode: number;
-                    areaCode: number;
-                    phoneNumber: number;
-                    phoneType: string;
-                    phoneTypeCode: number;
-                    updateDate: string;
-                }[];
-            };
-            negativeData: {
-                pefin: {
-                    pefinResponse: {
-                        occurrenceDate: string;
-                        legalNatureId: string;
-                        legalNature: string;
-                        contractId: string;
-                        creditorName: string;
-                        amount: number;
-                        principal: boolean;
-                        legalSquare: string;
-                        dispute: {
-                            disputeIndicativeFlag: boolean;
-                        };
-                        cadus: string;
-                    }[];
-                    summary: {
-                        count: number;
-                        balance: number;
-                        firstOccurrence: string;
-                        lastOccurrence: string;
-                    };
-                };
-                refin: {
-                    refinResponse: any[];
-                    summary: {
-                        count: number;
-                        balance: number;
-                    };
-                };
-                notary: {
-                    notaryResponse: any[];
-                    summary: {
-                        count: number;
-                        balance: number;
-                    };
-                };
-                check: {
-                    checkResponse: any[];
-                    summary: {
-                        count: number;
-                        balance: number;
-                    };
-                };
-                collectionRecords: {
-                    collectionRecordsResponse: any[];
-                    summary: {
-                        count: number;
-                        balance: number;
-                    };
-                };
-            };
-            score: {
-                score: number;
-                scoreModel: string;
-                range: string;
-                defaultRate: string;
-                codeMessage: number;
-                message: string;
-            };
-            facts: {
-                inquiry: {
-                    inquiryResponse: {
-                        occurrenceDate: string;
-                        segmentDescription: string;
-                        daysQuantity: number;
-                    }[];
-                    summary: {
-                        count: number;
-                    };
-                };
-                inquirySummary: {
-                    inquiryQuantity: {
-                        actual: number;
-                        checkActual: number;
-                        creditInquiriesQuantity: {
-                            inquiryDate: string;
-                            occurrences: number;
-                            bankOccurrences: number;
-                            companyOccurrences: number;
+            status: number;
+            code: string;
+            type: string;
+            message: string;
+            count: number;
+            results: {
+                servicesId?: string;
+                servicesTag?: string;
+                isApiService?: boolean;
+                status: string;
+                result?: {
+                    Cod: string;
+                    jsonData: {
+                        reports: {
+                            reportName: string;
+                            registration: {
+                                documentNumber: string;
+                                consumerName: string;
+                                motherName: string;
+                                consumerGender: string;
+                                birthDate: string;
+                                statusRegistration: string;
+                                statusDate: string;
+                                consumerGenderDescription: string;
+                                address: {
+                                    addressLine: string;
+                                    district: string;
+                                    zipCode: string;
+                                    country: string;
+                                    city: string;
+                                    state: string;
+                                };
+                                addresses: {
+                                    addressLine: string;
+                                    addressTypeCode: number;
+                                    addressTypeDescription: string;
+                                    addressNumber: string;
+                                    district: string;
+                                    zipCode: string;
+                                    country: string;
+                                    city: string;
+                                    state: string;
+                                    addressComplement: string;
+                                    updateDate: string;
+                                }[];
+                                phones: {
+                                    regionCode: number;
+                                    areaCode: number;
+                                    phoneNumber: number;
+                                    phoneType: string;
+                                    phoneTypeCode: number;
+                                    updateDate: string;
+                                }[];
+                            };
+                            negativeData: {
+                                pefin: {
+                                    pefinResponse: any[];
+                                    summary: { count: number; balance: number };
+                                };
+                                refin: {
+                                    refinResponse: any[];
+                                    summary: { count: number; balance: number };
+                                };
+                                notary: {
+                                    notaryResponse: any[];
+                                    summary: { count: number; balance: number };
+                                };
+                                check: {
+                                    checkResponse: any[];
+                                    summary: { count: number; balance: number };
+                                };
+                                collectionRecords: {
+                                    collectionRecordsResponse: any[];
+                                    summary: { count: number; balance: number };
+                                };
+                            };
+                            score: {
+                                score: number;
+                                scoreModel: string;
+                                range: string;
+                                defaultRate: string;
+                                codeMessage: number;
+                                message: string;
+                            };
+                            facts: {
+                                inquiry: {
+                                    inquiryResponse: {
+                                        occurrenceDate: string;
+                                        segmentDescription: string;
+                                        daysQuantity: number;
+                                    }[];
+                                    summary: { count: number };
+                                };
+                                inquirySummary: {
+                                    inquiryQuantity: {
+                                        actual: number;
+                                        checkActual: number;
+                                        creditInquiriesQuantity: {
+                                            inquiryDate: string;
+                                            occurrences: number;
+                                            bankOccurrences: number;
+                                            companyOccurrences: number;
+                                        }[];
+                                        checkInquiriesQuantity: {
+                                            inquiryDate: string;
+                                            occurrences: number;
+                                        }[];
+                                    };
+                                    summary: {
+                                        count: number;
+                                        checkCount: number;
+                                        creditCount: number;
+                                    };
+                                };
+                                stolenDocuments: {
+                                    stolenDocumentsResponse: any[];
+                                    summary: { count: number; balance: number };
+                                };
+                                judgementFilings: {
+                                    judgementFilingsResponse: any[];
+                                    summary: { count: number; balance: number };
+                                };
+                                bankrupts: {
+                                    bankruptsResponse: any[];
+                                    summary: { count: number; balance: number };
+                                };
+                            };
+                            partner: {
+                                partnershipResponse: {
+                                    businessDocument: string;
+                                    companyName: string;
+                                    participationPercentage: number;
+                                    companyStatus: string;
+                                    companyStatusCode: string;
+                                    companyState: string;
+                                    companyStatusDate: string;
+                                    updateDate: string;
+                                    participationInitialDate: string;
+                                    hasNegative: boolean;
+                                }[];
+                                summary: {
+                                    count: number;
+                                    balance: number;
+                                };
+                            };
+                            attributes: {
+                                attributesResponse: {
+                                    scoring: number;
+                                    attributeModel: string;
+                                    codeMessage: number;
+                                    message: string;
+                                }[];
+                            };
                         }[];
-                        checkInquiriesQuantity: {
-                            inquiryDate: string;
-                            occurrences: number;
-                        }[];
-                    };
-                    summary: {
-                        count: number;
-                        checkCount: number;
-                        creditCount: number;
                     };
                 };
-                stolenDocuments: {
-                    stolenDocumentsResponse: any[];
-                    summary: {
-                        count: number;
-                        balance: number;
-                    };
-                };
-                judgementFilings: {
-                    judgementFilingsResponse: any[];
-                    summary: {
-                        count: number;
-                        balance: number;
-                    };
-                };
-                bankrupts: {
-                    bankruptsResponse: any[];
-                    summary: {
-                        count: number;
-                        balance: number;
-                    };
-                };
-            };
-            partner: {
-                partnershipResponse: {
-                    businessDocument: string;
-                    companyName: string;
-                    participationPercentage: number;
-                    companyStatus: string;
-                    companyStatusCode: string;
-                    companyState: string;
-                    companyStatusDate: string;
-                    updateDate: string;
-                    participationInitialDate: string;
-                    hasNegative: boolean;
-                }[];
-                summary: {
-                    count: number;
-                    balance: number;
-                };
-            };
+                providedData?: string;
+                userId: string;
+                createDate: string;
+                updateDate: string;
+                previousServiceId?: string | null;
+                _id: string;
+                __v: number;
+                // Para o segundo objeto:
+                category?: string;
+                methodPayment?: string;
+                amount?: number;
+                dateStartTransaction?: string;
+                dateFinishedTransaction?: string | null;
+                type?: string;
+                isPositive?: boolean;
+                motive?: string;
+                matrix?: string;
+            }[]
         }
     }
 
