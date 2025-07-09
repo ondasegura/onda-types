364dcb076c76fe3b1908f245f3d0f546b00f9bd1
lenonbr <lenon.br@hotmail.com>
2025-07-09 13:21:13 -0300

    ÔÜí perf: adiciona valor em onda-types

--- src/backend-worker-financeiro/controllers/recebedor.ts
+++ src/backend-worker-financeiro/controllers/recebedor.ts
@@ -181,37 +181,12 @@ export namespace ControllerRecebedor {
     }
 
     export namespace AtualizarPeloId {
-        const AtualizazaoSchema = z4.object({
-            _id: z4.uuid(),
-            tipo: z4.union([z4.literal("individual"), z4.literal("empresa")]),
-            nome: z4.string(),
-            nome_mae: z4.string(),
-            data_nascimento: z4.string(),
-            renda_mensal: z4.number(),
-            ocupacao_profissional: z4.string(),
-            razao_social: z4.string(),
-            nome_fantasia: z4.string(),
-            faturamento_anual: z4.number(),
-            tipo_empresa: z4.string(),
-            data_fundacao: z4.string(),
-            email: z4.email(),
-            documento: z4.string(),
-            site: z4.string(),
-            telefones: z4.array(TelefoneBasicoSchema),
-            endereco: EnderecoCompletoSchema,
-            endereco_principal: EnderecoCompletoSchema,
-            socios_administradores: z4.array(SocioAdministradorSchema),
-            conta_bancaria: ContaBancariaSchema,
-            configuracoes_transferencia: ConfiguracoesTransferenciaSchema,
-            configuracoes_antecipacao: ConfiguracoesAntecipacaoSchema,
-            codigo: z4.string(),
-        });
 
         export const InputSchema = z4.object({
             data: z4.object({
                 recebedor: z4.object({
                     _id: z4.string(),
-                    atualizacao: AtualizazaoSchema.partial(),
+                    pagarme_recebedor_id: z4.string().optional().nullable()
                 }),
             }),
         });
