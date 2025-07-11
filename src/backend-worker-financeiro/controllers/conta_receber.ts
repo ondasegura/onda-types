c4e5bbdd8bb4e4cdf75b8f6757d7a8b647bba780
iJulians <silveirajuliano7991@gmail.com>
2025-07-11 16:11:17 -0300

    :recycle: refactor: adiciona descricao ao schema base de conta a receber

--- src/backend-worker-financeiro/controllers/conta_receber.ts
+++ src/backend-worker-financeiro/controllers/conta_receber.ts
@@ -46,6 +46,7 @@ namespace ControllerContaReceber {
         documento_titular: z4.string(),
         titular: z4.string(),
         status: z4.number(),
+        status_descricao: z4.string(),
         numero_cartao: z4.string(),
         numero_serial: z4.string(),
         pagamento_id: z4.string(),
