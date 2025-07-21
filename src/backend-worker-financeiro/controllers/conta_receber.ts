3d0deef641ee15ef5d0450a7f3f2516d34b21a68
ijulians <silveirajuliano7991@gmail.com>
2025-07-21 14:39:48 -0300

    :sparkles: feat: adiciona validação se o metodo de pagamento for pix e as parcelas forem maior que 1

--- src/backend-worker-financeiro/controllers/conta_receber.ts
+++ src/backend-worker-financeiro/controllers/conta_receber.ts
@@ -148,9 +148,9 @@ namespace ControllerContaReceber {
                     .refine(
                         (val) => {
                             const metodo_pagamento = Array.isArray(val.metodo_pagamento) && val.metodo_pagamento.includes("pix");
-                            const parcelas = val.parcelas == 1;
+                            const parcelas = val.parcelas === 1;
 
-                            return metodo_pagamento ?? parcelas;
+                            return metodo_pagamento && parcelas;
                         },
                         {
                             path: ["parcelas"],
